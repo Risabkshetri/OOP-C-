7d7cd8fbbc524f08c4f1e63982187e86c657152c
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-07 10:04:33 +0530

    lab2 error fixed and lab3 finished

--- lab/lab3.cpp
+++ lab/lab3.cpp
@@ -1,46 +1,110 @@
+// // // public specifier.
+
+// // #include<iostream>
+// // using namespace std;
+
+// // class animal{   
+// //     public:
+// //     string name;
+
+// //     void sound(){
+// //         cout << "I can bark, woof!, woof!" << endl;
+// //     }
+
+// // };
+
+// // int main(){
+// //     animal pet;
+// //     cout << "Enter your pet name: ";
+// //     cin >> pet.name;
+// //    pet.sound();
+// //}
 // // public specifier.
 
 // #include<iostream>
 // using namespace std;
 
 // class animal{   
-//     public:
+//     private:
 //     string name;
-
-//     void sound(){
-//         cout << "I can bark, woof!, woof!" << endl;
+//     public:
+//     void setPet(string  p){
+//         name = p;
+//     };
+//     string getPet(){
+//       return name;
 //     }
 
 // };
 
 // int main(){
 //     animal pet;
-//     cout << "Enter your pet name: ";
-//     cin >> pet.name;
-//    pet.sound();
-//}
-// public specifier.
+//     pet.setPet("dog");
+//     cout << pet.getPet() << " can bark, woof!, woof!" << endl;
 
-#include<iostream>
+
+// }
+
+
+
+#include <iostream>
 using namespace std;
 
-class animal{   
-    private:
+
+class Animal {
+public:
     string name;
-    public:
-    void setPet(string  p){
-        name = p;
-    };
-    string getPet(){
-      return name;
+
+   
+    Animal(string name1 = "") {
+        name = name1;
     }
 
+    void sound(string name1) {
+        cout << "I can bark, woof woof!" << endl;
+    }
+
+private:
+    string birdName;
+
+public:
+    
+    void setBird(string bird) {
+        birdName = bird;
+    }
+
+    
+    string getBird() {
+        return birdName;
+    }
+
+protected:
+    string domesticAnimalName;
 };
 
-int main(){
-    animal pet;
-    pet.setPet("dog");
-    cout << pet.getPet() << " can bark, woof!, woof!" << endl;
 
+class DomesticAnimal : public Animal {
+public:
+      void sound(string dom1) {
+        cout << "Cat sounds lovely." << endl;
+    }
+};
 
+// Main function
+int main() {
+
+    Animal myAnimal("Joy");
+
+   
+    Animal bird;
+    bird.setBird("parrot");
+    
+    
+    cout << "Bird is: " << bird.getBird() << endl;
+
+   
+    DomesticAnimal myPet;
+    myPet.sound("meow");
+
+    return 0;
 }
---
+++ lab/lab4.cpp
@@ -0,0 +1,24 @@
+// nested class demonstration
+#include<iostream> 
+using namespace std; 
+class Students {
+   public:             
+   class Friends {
+      private:
+      string contactNo;
+      public:
+      void getdata(string n) {
+        contactNo = n;
+      }
+      void putdata() {
+         cout << "My contact number is " << contactNo;
+      }
+   };
+};
+int main() {
+   cout<<"Nested classes in C++"<< endl;
+   Students :: Friends myFriend;
+   myFriend.getdata("+9139598765");
+   myFriend.putdata();
+   return 0;
+}

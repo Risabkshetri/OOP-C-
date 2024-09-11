567b58b52744f358131a29b1fd69191a65249ba4
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-11 23:30:29 +0530

    lab4 end here

--- lab/lab4.cpp
+++ lab/lab4.cpp
@@ -1,31 +1,31 @@
-// this is for testing purpose, need not to write on lab file!
-// nested class demonstration
-// #include<iostream> 
-// using namespace std; 
-// class Students {
-//    public:             
-//    class Friends {
-//       private:
-//       string contactNo;
-//       public:
-//       void getdata(string n) {
-//         contactNo = n;
-//       }
-//       void putdata() {
-//          cout << "My contact number is " << contactNo;
-//       }
-//    };
-// };
-// int main() {
-//    cout<<"Nested classes in C++"<< endl;
-//    Students :: Friends myFriend;
-//    myFriend.getdata("+9139598765");
-//    myFriend.putdata();
-//    return 0;
-// }
+// // this is for testing purpose, need not to write on lab file!
+//nested class demonstration
+#include<iostream> 
+using namespace std; 
+class Students {
+   public:             
+   class Friends {
+      private:
+      string contactNo;
+      public:
+      void getdata(string n) {
+        contactNo = n;
+      }
+      void putdata() {
+         cout << "My contact number is " << contactNo;
+      }
+   };
+};
+int main() {
+   cout<<"Nested classes in C++"<< endl;
+   Students :: Friends myFriend;
+   myFriend.getdata("+9139598765");
+   myFriend.putdata();
+   return 0;
+}
 
 
-// friend function demonstration
+// // friend function demonstration
 
 #include<iostream>
 using namespace std;
@@ -35,11 +35,7 @@ class Students {
       string name;
 
    public:
-      
-      Students(string studentName) : name(studentName) {}
-
-      
-      friend void showStudentName(Students& s);
+      Students(string studentName) : name(studentName) {}friend void showStudentName(Students& s);
 };
 
 void showStudentName(Students& s) {

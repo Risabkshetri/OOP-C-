e8784336f03145f7457cc340170ef1101487bc00
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-18 21:37:05 +0530

    small practice

--- lab/tempCodeRunnerFile.cpp
+++ lab/tempCodeRunnerFile.cpp
@@ -1,31 +1,75 @@
+// // #include<iostream>
+// // using namespace std;
+
+// // class Calcultion {
+// //     protected:
+// //     int num1 = 10, num2 = 20;
+
+// //     public:
+// //     void operations(){
+// //         cout << "sum = " << num1 + num2 << endl;
+// //     }
+// // };
+
+// // class multiplication : public Calcultion{
+// //     public:
+// //     void operations(){
+// //         cout << "Multiplication : " << num1*num2 << endl;
+// //     }
+// // };
+// // int main(){
+// //    Calcultion c;
+// //    multiplication m;
+// //    m.operations();
+// //    c.operations();
+// // }
+
+
+// // implementation of virtual function
+
 // #include<iostream>
 // using namespace std;
 
-// class Calcultion {
-//     protected:
-//     int num1 = 10, num2 = 20;
-
+// class Base{
 //     public:
-//     void operations(){
-//         cout << "sum = " << num1 + num2 << endl;
+//     int num;
+
+//     virtual void setmember(){
+//         cout << "Enter the value of x : ";
+//         cin >> num;
+//     }
+//     virtual void disp(){
+//         cout << num << " is member of base class." << endl;
+        
+//     }
+
+// };
+
+// class Derived_class : public Base{
+//     public:
+//     void setmember() override{
+//         cout << "Enter value of x : ";
+//         cin >> num;
+//     }
+//     void disp() override{
+//         cout << num << " is member of derived class";
 //     }
 // };
 
-// class multiplication : public Calcultion{
-//     public:
-//     void operations(){
-//         cout << "Multiplication : " << num1*num2 << endl;
-//     }
-// };
 // int main(){
-//    Calcultion c;
-//    multiplication m;
-//    m.operations();
-//    c.operations();
+//     Base b;
+//     Derived_class d;
+
+//     b.setmember();
+//     b.disp();
+
+//     d.setmember();
+//     d.disp();
+
+//     return 0;
 // }
 
-
-// implementation of virtual function
+// constructor overloading
 
 #include<iostream>
 using namespace std;
@@ -34,37 +78,12 @@ class Base{
     public:
     int num;
 
-    virtual void setmember(){
+    Base(){
         cout << "Enter the value of x : ";
-        cin >> num;
-    }
-    virtual void disp(){
-        cout << num << " is member of base class." << endl;
-        
+        cin >> num; 
     }
 
-};
-
-class Derived_class : public Base{
-    public:
-    void setmember() override{
-        cout << "Enter value of x : ";
-        cin >> num;
+    Base(int x){
+        num = x;
     }
-    void disp() override{
-        cout << num << " is member of derived class";
-    }
-};
-
-int main(){
-    Base b;
-    Derived_class d;
-
-    b.setmember();
-    b.disp();
-
-    d.setmember();
-    d.disp();
-
-    return 0;
-}
+};  

cee83a5ac228fd83a240207aa7043540a5a2e586
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-27 16:47:22 +0530

    vector data structure

---
+++ DSA/vector.cpp
@@ -0,0 +1,23 @@
+// vector is basically resizable array
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    int n;
+    cout << "Enter size of array : " << endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    cout<<"Array : "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}

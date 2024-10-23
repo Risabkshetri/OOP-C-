e70a95bad8256cd72e99753a1be4939ec981cba0
risabkshetri <risawgc@gmail.com>
2024-10-23 18:46:07 +0530

    lab9 end her

--- lab/lap9.cpp
+++ lab/lap9.cpp
@@ -1,37 +1,75 @@
 // C++ Program to implement
 // template Array class
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// template <typename T> class Array {
+// private:
+//     T* ptr;
+//     int size;
+
+// public:
+//     Array(T arr[], int s);
+//     void print();
+// };
+
+// template <typename T> Array<T>::Array(T arr[], int s)
+// {
+//     ptr = new T[s];
+//     size = s;
+//     for (int i = 0; i < size; i++)
+//         ptr[i] = arr[i];
+// }
+
+// template <typename T> void Array<T>::print()
+// {
+//     for (int i = 0; i < size; i++)
+//         cout << " " << *(ptr + i);
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     int arr[5] = { 1, 2, 3, 4, 5 };
+//     Array<int> a(arr, 5);
+//     a.print();
+//     return 0;
+// }
+
+
+// optimized and simple code
+#include<iostream>
 using namespace std;
 
-template <typename T> class Array {
-private:
-    T* ptr;
-    int size;
-
-public:
-    Array(T arr[], int s);
-    void print();
+template <typename T> 
+class Array{
+    private:
+       T* ptr;
+       int size;
+    
+    public:
+       Array(T arr[], int s){
+           size = s;
+           ptr = new T[s];
+           for(int i = 0; i<size; i++){
+               ptr[i] = arr[i];
+           };
+       };
+       
+       void print(){
+           for(int i = 0; i<size; i++){
+               cout << ptr[i] << " ";
+           };
+       };
+       
+       ~Array(){
+           delete[] ptr;
+       };
 };
 
-template <typename T> Array<T>::Array(T arr[], int s)
-{
-    ptr = new T[s];
-    size = s;
-    for (int i = 0; i < size; i++)
-        ptr[i] = arr[i];
-}
-
-template <typename T> void Array<T>::print()
-{
-    for (int i = 0; i < size; i++)
-        cout << " " << *(ptr + i);
-    cout << endl;
-}
-
-int main()
-{
-    int arr[5] = { 1, 2, 3, 4, 5 };
+int main(){
+    int arr[5] = {1,2,3,4,5};
     Array<int> a(arr, 5);
     a.print();
-    return 0;
+    return 0 ;
 }
